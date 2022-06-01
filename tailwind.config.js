ea8f4403f437b106164453060cf20912e8490cce
Hayman <hayman.yoonhanthar@gmail.com>
2022-06-01 16:31:58 +0630

    tailwindTest

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        primary:'#FF6363',
+        secondary:{
+          100:'#E2E2D5',
+          200:'#888883'
+        },
+        blurcolor:{
+          100:'#ce616190',
+          200:'#95888890',
+          300:'#6168ce90'
+        }
+      },
+      marginTop:{
+        marginminus :'-100px!important',
+      }
+    },
+  },
+  plugins: [],
+}
